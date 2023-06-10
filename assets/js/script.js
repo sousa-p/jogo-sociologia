c9dfb88a01a55f73e48e1cfa839062747349fed5
sousa-p <p.soousa7@gmail.com>
2023-06-09 23:04:34 -0300

    Feature movimentação, fonte, limpar anuncios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 const { createApp } = Vue
 
 const app = createApp({
-  components: {Anuncios},
   data() {
     return {
       player: {
@@ -9,20 +8,41 @@ const app = createApp({
         score: 0,
         x: 0,
         y: 0,
-        sprt: '',
+        sprt: 'assets/sprts/mario.gif',
+
+        mudarPosicao() {
+          this.x = 90*Math.random()+3;
+          this.y = 90*Math.random()+3;
+        }
       },
-      anuncios: []
+      anuncios: [],
+      btnLimparAnuncios: false
     }
   },
   methods: {
-    criarAnuncios (qtdd) {
-      for(let i=0; i<qtdd; i++) {
-        this.anuncios.push(new Anuncio(10+(i*(100/qtdd)), 10+(i*(100/qtdd)), Math.floor(10/(this.player.score+1)*Math.random()), 'a'))
+    criarAnuncios(qtdd=1) {
+      for(let i=1; i<=qtdd; i++) {
+        this.anuncios.push(
+          {
+            y: 100*Math.random(),
+            velocidade: Math.floor(10/(this.player.score+1)*(Math.random()+0.2)) + 1,
+          }
+        )
       }
+    },
+    aumentarScore() {
+      this.player.score++;
+      this.criarAnuncios();
+      if (this.player.score % 10 === 0) this.btnLimparAnuncios = true;
+    },
+    limparAnuncios(qtdd=this.anuncios.length/4) {
+      this.anuncios.splice(0, qtdd);
+      this.btnLimparAnuncios = false;
     }
   }
   ,
-  mounted() {
+  created() {
+    this.player.mudarPosicao();
     this.criarAnuncios(this.player.score + 5);
   }
 })
