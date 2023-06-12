1a66ad1c68eca52ed3e091069f1f9d4127b6bcd3
sousa-p <p.soousa7@gmail.com>
2023-06-11 23:18:12 -0300

    Refatoração e melhoramento de desempenho

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,107 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 const app = createApp({
   data() {
     return {
-      sounds: {
-        gameMusic: new Audio('./assets/audio/music_game.mpeg'),
-        anuncio: new Audio('./assets/audio/anuncio.mp3'),
-        start: new Audio('./assets/audio/start.mp3'),
-        player: new Audio('./assets/audio/player.mp3'),
-        limparAnuncios: new Audio('./assets/audio/power-up.wav')
+      partidaOcorrendo: false,
+      mostrarGameOver: false,
+      mostrarLimparAnuncios: false,
+      sons: {
+        partida: new Audio("./assets/audio/partida.mpeg"),
+        anuncio: new Audio("./assets/audio/anuncio.mp3"),
+        comecar: new Audio("./assets/audio/comecar.mp3"),
+        livro: new Audio("./assets/audio/livro.mp3"),
+        limparAnuncios: new Audio("./assets/audio/limparAnuncios.wav"),
+        gameOver: new Audio("./assets/audio/gameOver.wav"),
       },
-      gameOcorrendo: false,
-      gameOverPage: false,
-      player: {
+      jogador: {
         vida: 100,
         score: 0,
-        x: 0,
-        y: 0,
-        sprt: 'assets/sprts/livro-sprite.gif',
-        aumentarVida(qtdd=5) {
-          (this.vida + qtdd <= 100)
-            ? this.vida += qtdd
-            : this.vida += 100 - this.vida;
+        aumentarVida(qtdd = 5) {
+          this.vida + qtdd <= 100
+            ? (this.vida += qtdd)
+            : (this.vida += 100 - this.vida);
         },
-        mudarPosicao() {
-          this.x = 75*Math.random()+3;
-          this.y = 75*Math.random()+3;
-        },
-        reset () {
+        resetStatus() {
           this.vida = 100;
           this.score = 0;
-          this.mudarPosicao();
-        }
+        },
+      },
+      livro: {
+        x: 0,
+        y: 0,
+        mudarPosicao() {
+          this.x = 75 * Math.random() + 3;
+          this.y = 75 * Math.random() + 3;
+        },
       },
       anuncios: [],
-      btnLimparAnuncios: false
-    }
+    };
+  },
+  created () {
+    this.sons.partida.volume = 0.5;
+    this.sons.gameOver.volume = 0.5;
   },
   methods: {
-    startGame () {
-      this.sounds.start.currentTime = 1;
-      this.sounds.start.play();
-      this.player.reset();
+    comecarPartida() {
+      this.sons.comecar.currentTime = 1;
+      this.sons.comecar.play();
+      this.jogador.resetStatus();
+      this.livro.mudarPosicao();
       this.criarAnuncios(5);
       setTimeout(() => {
-        this.sounds.gameMusic.volume = 0.5;
-        this.sounds.gameMusic.play();
-        this.gameOcorrendo = setInterval(() => {
-          this.player.vida -= 1;
-          if (this.player.vida <= 0) { this.gameOver() };
+        this.sons.partida.play();
+        this.partidaOcorrendo = setInterval(() => {
+          this.jogador.vida -= 1;
+          if (this.jogador.vida <= 0) {
+            this.gameOver();
+          }
         }, 100);
       }, 500);
     },
-    criarAnuncios(qtdd=1) {
-      for(let i=1; i<=qtdd; i++) {
-        this.anuncios.push(
-          {
-            y: 70*Math.random() + 15,
-            imgId: Math.ceil(Math.random()*7),
-            tamanho: Math.floor((Math.random()*30) + 50),
-            velocidade: Math.floor(Math.random()*5) + 1,
-          }
-        )
+    criarAnuncios(qtdd = 1) {
+      for (let i = 1; i <= qtdd; i++) {
+        this.anuncios.push({
+          y: 70 * Math.random() + 15,
+          imgId: Math.ceil(Math.random() * 7),
+          tamanho: Math.floor(Math.random() * 30 + 50),
+          velocidade: Math.floor(Math.random() * 5) + 1,
+        });
       }
     },
     clickAnuncio() {
-      this.player.aumentarVida(-10);
-      this.sounds.anuncio.currentTime = 0.25;
-      this.sounds.anuncio.play();
+      this.jogador.aumentarVida(-10);
+      this.sons.anuncio.currentTime = 0.25;
+      this.sons.anuncio.play();
     },
-    clickPlayer() {
-      this.sounds.player.currentTime = 0;
-      this.sounds.player.play();
-      this.player.aumentarVida();
+    clickLivro() {
+      this.jogador.aumentarVida();
       this.aumentarScore();
-      this.player.mudarPosicao();
+      this.livro.mudarPosicao();
+      this.sons.livro.currentTime = 0;
+      this.sons.livro.play();
     },
-    aumentarScore() {
-      this.player.score++;
-      if (this.player.score % 2 === 0) this.criarAnuncios();
-      if (this.player.score % 10 === 0) this.btnLimparAnuncios = true;
+    aumentarScore(qtdd = 1) {
+      this.jogador.score += qtdd;
+      if (this.jogador.score % 2 === 0) this.criarAnuncios();
+      if (this.jogador.score % 10 === 0) this.mostrarLimparAnuncios = true;
     },
-    limparAnuncios(qtdd=this.anuncios.length/2.5) {
+    limparAnuncios(qtdd = this.anuncios.length / 2.5) {
       this.anuncios.splice(0, qtdd);
-      this.sounds.limparAnuncios.play();
-      this.btnLimparAnuncios = false;
+      this.sons.limparAnuncios.play();
+      this.mostrarLimparAnuncios = false;
     },
-    gameOver () {
-      this.sounds.gameMusic.pause();
-      this.sounds.gameMusic.currentTime = 0;
-      clearInterval(this.gameOcorrendo);
-      this.gameOcorrendo = false;
-      this.btnLimparAnuncios = false;
-      this.anuncios = []
-      this.player.reset();
-      this.gameOverPage = true;
-    }
-  }
-})
+    gameOver() {
+      clearInterval(this.partidaOcorrendo);
+      this.partidaOcorrendo = false;
+      this.sons.partida.pause();
+      this.sons.partida.currentTime = 0;
+      this.sons.gameOver.play();
+      this.mostrarLimparAnuncios = false;
+      this.anuncios = [];
+      this.mostrarGameOver = true;
+    },
+  },
+});
 
-app.mount('#app');
+app.mount("#app");
